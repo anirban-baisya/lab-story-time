c8428f609043a2729717e3b7e4a35bd3be7f8cbc
anirban-baisya <anirban.baisya2020@vitbhopal.ac.in>
2021-09-15 06:12:37 +0530

    First Commit

--- src/app.js
+++ src/app.js
@@ -2,15 +2,45 @@
 //We've got some basic info about Karen's home
 //Debug the type of data provided
 //Return the types concatenated in a single variable
+function moreAboutHome(address, distanceFromTown, hasNeighbours) {
+    return (result =
+        typeof address + typeof distanceFromTown + typeof hasNeighbours);
+}
 
 //Progression 2:
 //Check if the data given is of the right type
 //parents = String, noOfSiblings = Number, isNuclearFamily = Boolean
+function moreAboutKaren(parents, noOfSiblings, isNuclearFamily) {
+    if (
+        typeof parents === "string" &&
+        typeof noOfSiblings === "number" &&
+        typeof isNuclearFamily === "boolean"
+    ) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 //Progression 3:
 //Lily is suspicious about Karen's new friend
 //Karen tells her friend's age and even writes it down
 //Check which one those is not a number (NaN) and return that value
+function doesFriendExist(ageInText, ageInNumber) {
+    if (isNaN(ageInText) === true) {
+        return ageInText;
+
+        /*
+           The Number.isNaN() method determines whether a value is NaN (Not-A-Number).
+            This method returns true if the value is of the type Number, and equates to NaN. Otherwise it returns false.
+            Number.isNaN() is different from the global isNaN() function. The global isNaN() function converts the tested value to a Number, then tests it.
+            Number.isNaN() does not convert the values to a Number, and will not return true for any value that is not of the type Number.
+           */
+    }
+    if (isNaN(ageInNumber) === true) {
+        return ageInNumber;
+    }
+}
 
 //Progression 4:
 //Lily gave Karen x sweets
@@ -19,9 +49,27 @@
 //Her friend divided the remaining sweets into 2 parts for each
 //How many sweets did her friend get to eat?
 
+function sweetTooth(totalNoOfSweets, sweetsConsumedByKaren, sweetsConsumedInNMeters, metersToTravel) {
+    if ((typeof (totalNoOfSweets) == "number" && typeof (sweetsConsumedByKaren) == "number" && typeof (sweetsConsumedInNMeters) == "number" && typeof (metersToTravel) == "number")) {
+        var remaining = (totalNoOfSweets - (sweetsConsumedByKaren + sweetsConsumedInNMeters + metersToTravel)) / 2;
+
+        return remaining;
+    } else
+        return 'No sweets for Karen\'s friend';
+}
 //Progression 5:
 //As Lily moves closer, it gets colder. She checks the temperature on her mobile
 //It only shows in fahrenheit. Convert the data to celsius and return it.
+function convertToCelsius(fahrenheit) {
+    if (typeof (fahrenheit) == "string" || typeof (fahrenheit) == "undefined" || typeof (fahrenheit) == "object") {
+        return 'Technical Error!';
+    } else {
+        var celsius = (fahrenheit - 32) * 5 / 9
+        return celsius;
+    }
+}
+
+
 
 //Progression 6:
 //Lily can now do multiple things to deal with this
@@ -30,6 +78,23 @@
 //3. Counsel her daughter herself
 //4. Lock her daughter in her room
 //Given a value, return which of these above actions Lily would take
+function aDifficultChoice(choice) {
+    switch (choice) {
+        case 1:
+            return 'Take her daughter to a doctor';
+            break;
+        case -1:
+            return 'Break down and give up all hope';
+            break;
+        case undefined:
+            return "Wasn't able to decide";
+            break;
+        case "I give up":
+            return "Refused to do anything for Karen";
+            break;
+    }
+}
+
 
 //Progression 7:
 //Lily realized that she'd hurt her daughter
@@ -39,3 +104,11 @@
 //Take all of Lily's strategies and concatenate them to a single var
 //Seperate the strategies by a single space
 //Return the length of the complete strategy
+
+function aDifficultChoice(strategie1 , strategie2) {
+//strategie1 = 'adsdasd';
+// strategie2 = 'fsfsf';
+    var concaSrtategis = strategie1.concat(strategie2)
+    // console.log(concaSrtategis)
+    return concaSrtategis.length;
+}
